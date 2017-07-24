8179deca37387a622cb8520c676e9076512bb1bc
André <andre.iske@mailbox.org>
2017-07-24 19:33:23 +0200

    Added test dependencies for spring junit tests

--- src/test/java/de/iske/fratcher/user/UserServiceTest.java
+++ src/test/java/de/iske/fratcher/user/UserServiceTest.java
@@ -1,4 +1,15 @@
 package de.iske.fratcher.user;
 
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+// Use Spring's testing support in JUnit
+@RunWith(SpringRunner.class)
+// Enable Spring features, e.g. loading of application-properties, etc.
+@SpringBootTest
 public class UserServiceTest {
+
+
 }
