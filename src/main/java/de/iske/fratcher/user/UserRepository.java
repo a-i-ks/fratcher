3ca9cb5837e4e876be20e66029888904e8037d6b
André <andre.iske@mailbox.org>
2017-07-24 19:26:16 +0200

    Added UserService and UserRepository adopted from web-engineering demo project

---
+++ src/main/java/de/iske/fratcher/user/UserRepository.java
@@ -0,0 +1,13 @@
+package de.iske.fratcher.user;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+    @Query("SELECT u FROM User_ u WHERE u.email = :email")
+    User findByEmail(@Param("email") String email);
+
+    @Query("SELECT u from User_ u WHERE u.email = :email AND u.password = :password")
+    User findByEmailAndPassword(@Param("email") String email, @Param("password") String password);
+}
