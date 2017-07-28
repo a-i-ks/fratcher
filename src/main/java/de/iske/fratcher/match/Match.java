4b04905bd8e0e980911d735233fccb79d92c469f
André <andre.iske@mailbox.org>
2017-07-28 16:27:45 +0200

    Changed matchedUsers from list to SimpleEntry tuple

--- src/main/java/de/iske/fratcher/match/Match.java
+++ src/main/java/de/iske/fratcher/match/Match.java
@@ -3,23 +3,23 @@ package de.iske.fratcher.match;
 import de.iske.fratcher.user.User;
 
 import java.time.Instant;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.AbstractMap;
+import java.util.Map;
 
 public class Match {
 
-    private final List<User> matchedUsers;
+
+
+    private final Map.Entry<User,User> matchedUsers;
 
     private final Instant matchingTimestamp;
 
     public Match(User u1, User u2) {
-        matchedUsers = new ArrayList<>();
-        matchedUsers.add(u1);
-        matchedUsers.add(u2);
+        matchedUsers = new AbstractMap.SimpleEntry<User, User>(u1,u2);
         matchingTimestamp = Instant.now();
     }
 
-    public List<User> getMatchedUsers() {
+    public Map.Entry<User,User> getMatchedUsers() {
         return matchedUsers;
     }
 
