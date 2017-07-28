1f5c878f2fcbbe668fd2986919c4365e8bebd56c
André <andre.iske@mailbox.org>
2017-07-28 13:46:55 +0200

    Added pojo for created user object

--- src/main/java/de/iske/fratcher/user/UserController.java
+++ src/main/java/de/iske/fratcher/user/UserController.java
@@ -15,6 +15,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
 
+    private static class UserCreated {
+        public String url;
+    }
+
     @Autowired
     private UserService userService;
 
