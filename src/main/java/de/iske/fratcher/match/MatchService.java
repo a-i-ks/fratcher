d009c6e9dddf9341d3f840e3e107ffe720be4700
André <andre.iske@mailbox.org>
2017-08-02 00:03:50 +0200

    Added @Service annotation to MatchService class

--- src/main/java/de/iske/fratcher/match/MatchService.java
+++ src/main/java/de/iske/fratcher/match/MatchService.java
@@ -2,12 +2,14 @@ package de.iske.fratcher.match;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 /**
  * Service for all match-related functional operations.
  * @author André Iske
  * @since 2017-08-01
  */
+@Service
 public class MatchService {
 
     @Autowired
