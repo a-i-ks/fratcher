d46ebe083dfd25d2f9b61ebc32f30c61c45def9f
André <andre.iske@mailbox.org>
2017-08-01 23:42:35 +0200

    Added empty MatchController class

---
+++ src/main/java/de/iske/fratcher/match/MatchController.java
@@ -0,0 +1,12 @@
+package de.iske.fratcher.match;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * HTTP endpoint for a match-related HTTP requests.
+ * @author André Iske
+ * @since 2017-08-01
+ */
+@RestController
+public class MatchController {
+}
