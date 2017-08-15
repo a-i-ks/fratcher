5a7493b4cfbb87b099762cbde9249d44e2b06932
André Iske <andre.iske@mailbox.org>
2017-08-15 18:09:16 +0200

    Removed unused imports

--- src/main/java/de/iske/fratcher/user/UserRepository.java
+++ src/main/java/de/iske/fratcher/user/UserRepository.java
@@ -4,8 +4,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
-import java.util.stream.Stream;
-
 public interface UserRepository extends CrudRepository<User, Long> {
     @Query("SELECT u FROM User_ u WHERE u.email = :email")
     User findByEmail(@Param("email") String email);
