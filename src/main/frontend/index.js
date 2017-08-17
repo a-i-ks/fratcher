cbf90477693641cf101fcafbf0bb9aed1c2411a2
André Iske <andre.iske@mailbox.org>
2017-08-17 18:23:02 +0200

    Added index.js with some example code

---
+++ src/main/frontend/index.js
@@ -0,0 +1,13 @@
+var Greeter = React.createClass({
+    render: function () {
+        return <h1>Hello, {this.props.message}!</h1>
+    }
+});
+
+
+ReactDOM.render(
+    <div>
+        <Greeter message="Students"/>
+        <Greeter message="Michael"/>
+    </div>,
+    document.getElementById('root'));
