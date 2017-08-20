5c71ae82b395e0ebc7cdf39277a1c1b7a938dd51
André Iske <andre.iske@mailbox.org>
2017-08-20 19:38:48 +0200

    Moved user related requests to UserController class and leave only authentication related requests in AuthenticationController.

--- src/main/java/de/iske/fratcher/authentication/AuthenticationController.java
+++ src/main/java/de/iske/fratcher/authentication/AuthenticationController.java
@@ -1,13 +1,13 @@
 package de.iske.fratcher.authentication;
 
 
-import de.iske.fratcher.user.User;
-import de.iske.fratcher.user.UserService;
-import de.iske.fratcher.util.AddressService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * HTTP endpoint for a user-related HTTP requests.
@@ -15,19 +15,9 @@ import org.springframework.web.bind.annotation.*;
  * @since 2017-07-24
  */
 @RestController
-@RequestMapping("/api/user")
+@RequestMapping("/api/login")
 public class AuthenticationController {
-    @RequestMapping(value = "", method = RequestMethod.POST)
-    public ResponseEntity<Object> addUser(@RequestBody User user) {
-        userService.addUser(user);
-        UserCreated userCreated = new UserCreated(user, addressService.getServerURL());
-        return ResponseEntity.ok(userCreated);
-    }
 
-    @RequestMapping(value = "", method = RequestMethod.GET)
-    public Iterable<User> getUserList() {
-        return userService.getUserList();
-    }
 
     @Autowired
     private AuthenticationService authenticationService;
@@ -41,29 +31,13 @@ public class AuthenticationController {
         return new ResponseEntity<>(token, HttpStatus.OK);
     }
 
-    @Autowired
-    private AddressService addressService;
-
-    @Autowired
-    private UserService userService;
-
     public static class UserLogin {
         public String email;
         public String password;
     }
 
-    private static class UserCreated {
-        public String url;
 
-        public UserCreated(User user, String serverUrl) {
-            this.url = serverUrl + "/api/user/" + user.getId();
-        }
-    }
 
-    @RequestMapping(value = "{id}", method = RequestMethod.GET)
-    public User getUser(@PathVariable Long id) {
-        return userService.getUser(id);
-    }
 
 
 }
---
+++ src/main/java/de/iske/fratcher/user/UserController.java
@@ -0,0 +1,52 @@
+package de.iske.fratcher.user;
+
+import de.iske.fratcher.util.AddressService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+
+    @Autowired
+    private AddressService addressService;
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping(value = "{id}", method = RequestMethod.GET)
+    public ResponseEntity<User> getUser(@PathVariable Long id) {
+        if (userService.isAnonymous()) {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+        return new ResponseEntity<>(userService.getUser(id), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "", method = RequestMethod.POST)
+    public ResponseEntity<Object> addUser(@RequestBody User user) {
+        if (userService.isAnonymous()) {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+
+        userService.addUser(user);
+        UserCreated userCreated = new UserCreated(user, addressService.getServerURL());
+        return ResponseEntity.ok(userCreated);
+    }
+
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public ResponseEntity<Object> getUserList() {
+        if (userService.isAnonymous()) {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+        return new ResponseEntity<>(userService.getUserList(), HttpStatus.OK);
+    }
+
+    private static class UserCreated {
+        public String url;
+
+        public UserCreated(User user, String serverUrl) {
+            this.url = serverUrl + "/api/user/" + user.getId();
+        }
+    }
+}
