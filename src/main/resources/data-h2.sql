99e2d3d144f742c9b32e070dbad58841127df39d
André Iske <andre.iske@mailbox.org>
2017-08-20 19:36:01 +0200

    Initialize h2 database on startup

---
+++ src/main/resources/data-h2.sql
@@ -0,0 +1,17 @@
+-- Initialize database on startup. See
+-- https://docs.spring.io/spring-boot/docs/current/reference/html/howto-database-initialization.html#howto-initialize-a-database-using-spring-jdbc
+-- for explanation. This is a cool spring feature :-).
+
+
+-- Remove everything.
+DELETE FROM USER_;
+DELETE FROM MATCH_;
+
+-- Insert new users.
+INSERT INTO USER_ (id, username, email, PASSWORD) VALUES
+  (1, 'admin', 'admin@bla.de',
+   'fed6f5aa981718b738d05255469c84a0783007dee7aadda9549aa37a0127f3bcd920789359952f906fc8c499666f05da667edc2e879ad4b06e0e55b153eba5c5'),
+  (2, 'user', 'bla@bla.de',
+   '313dee0b8b9b98060fbf183249811ef45fbef0a5843c5f4a97bb65d678bc42cb0251a8753f3b735b0e8e795cf6472cac10b3a59629e7cb87a2282c14953dffb6'); -- bar
+
+
