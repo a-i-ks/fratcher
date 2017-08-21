a22cf9bd486fb93ccfe89564f7e37815207bfd1b
André Iske <andre.iske@mailbox.org>
2017-08-22 00:59:19 +0200

    Changed salted hash of password of demo user to correct value (forgot to add -n to shasum command)

--- src/main/resources/data-h2.sql
+++ src/main/resources/data-h2.sql
@@ -10,7 +10,7 @@ DELETE FROM MATCH_;
 -- Insert new users.
 INSERT INTO USER_ (id, username, email, PASSWORD) VALUES
   (1, 'admin', 'admin@bla.de',
-   'fed6f5aa981718b738d05255469c84a0783007dee7aadda9549aa37a0127f3bcd920789359952f906fc8c499666f05da667edc2e879ad4b06e0e55b153eba5c5'),
+   '4cca4c0e9fd213f4bd3c9f8aa997fdc8ea346944a607087ec1e5dd176b54c8abdb3fa4d1bd44c9835460d52f4fc5ca5b57ef906fab08785981508e525da2c145'),
   (2, 'user', 'bla@bla.de',
    '313dee0b8b9b98060fbf183249811ef45fbef0a5843c5f4a97bb65d678bc42cb0251a8753f3b735b0e8e795cf6472cac10b3a59629e7cb87a2282c14953dffb6'); -- bar
 
