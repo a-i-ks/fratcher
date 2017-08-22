897646497e807e8c7fb46bdd412bdb00c3e00c9d
André Iske <andre.iske@mailbox.org>
2017-08-22 14:58:49 +0200

    Added constructor for setting values quicker

--- src/main/java/de/iske/fratcher/authentication/UserLogin.java
+++ src/main/java/de/iske/fratcher/authentication/UserLogin.java
@@ -3,4 +3,9 @@ package de.iske.fratcher.authentication;
 public class UserLogin {
     public String email;
     public String password;
+
+    public UserLogin(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
 }
