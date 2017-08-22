229f2e6f467945935f3ff0d0458724bac9c3cbef
André Iske <andre.iske@mailbox.org>
2017-08-22 15:41:21 +0200

    Added missing default constructor (needed for JPA)

--- src/main/java/de/iske/fratcher/authentication/UserLogin.java
+++ src/main/java/de/iske/fratcher/authentication/UserLogin.java
@@ -4,6 +4,9 @@ public class UserLogin {
     public String email;
     public String password;
 
+    public UserLogin() {
+    }
+
     public UserLogin(String email, String password) {
         this.email = email;
         this.password = password;
