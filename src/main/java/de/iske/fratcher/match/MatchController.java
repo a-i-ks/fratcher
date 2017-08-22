027d167377655923c80b40220c5ea56b8f9f00f0
André Iske <andre.iske@mailbox.org>
2017-08-22 14:54:40 +0200

    Added possibility to match users

--- src/main/java/de/iske/fratcher/match/MatchController.java
+++ src/main/java/de/iske/fratcher/match/MatchController.java
@@ -1,5 +1,16 @@
 package de.iske.fratcher.match;
 
+import de.iske.fratcher.user.User;
+import de.iske.fratcher.user.UserService;
+import de.iske.fratcher.util.AddressService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -8,5 +19,46 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2017-08-01
  */
 @RestController
+@RequestMapping("/api/match")
 public class MatchController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(MatchController.class);
+
+
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    MatchService matchService;
+
+    @Autowired
+    AddressService addressService;
+
+    /**
+     * This REST endpoint is used to "like" another user
+     *
+     * @param likedUser - The user that the current "likes"
+     * @return MatchCreated object with url of new match and
+     */
+    @RequestMapping(value = "", method = RequestMethod.POST)
+    public ResponseEntity<Object> addMatch(@RequestBody User likedUser) {
+        if (userService.isAnonymous()) {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+        Match match = matchService.likeUser(likedUser);
+        MatchCreated matchCreated = new MatchCreated(match, addressService.getServerURL());
+        return ResponseEntity.ok(matchCreated);
+    }
+
+    private static class MatchCreated {
+        public String url;
+
+        public boolean confirmed;
+
+        public MatchCreated(Match match, String url) {
+            this.url = url + "/api/match/" + match.getId();
+            this.confirmed = match.isConfirmed();
+        }
+    }
+
 }
--- src/main/java/de/iske/fratcher/match/MatchService.java
+++ src/main/java/de/iske/fratcher/match/MatchService.java
@@ -1,8 +1,15 @@
 package de.iske.fratcher.match;
 
 
+import de.iske.fratcher.user.User;
+import de.iske.fratcher.user.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 /**
  * Service for all match-related functional operations.
@@ -12,11 +19,40 @@ import org.springframework.stereotype.Service;
 @Service
 public class MatchService {
 
+    private static final Logger LOG = LoggerFactory.getLogger(MatchService.class);
+
+    @Autowired
+    private UserService userService;
+
     @Autowired
     private MatchRepository matchRepository;
 
+    @PersistenceContext
+    private EntityManager entityManager;
+
     public void addMatch(Match match) {
         matchRepository.save(match);
+        LOG.info(match.getUser1().getUsername());
     }
 
+    /**
+     * Method that is called if one user likes another.
+     *
+     * @param likedUser - the user who has been liked by initialUser
+     * @return - the match object that has either been created or was already existing
+     */
+    public Match likeUser(User likedUser) {
+        User initialUser = userService.getCurrentUser();
+        //check if likedUser has already like initialUser
+        for (Match match : likedUser.getMatches()) {
+            if (match.getUser2().equals(initialUser)) {
+                match.confirm();
+                return match; // it's a match
+            }
+        }
+        Match unconfirmedMatch = new Match();
+        unconfirmedMatch.setUsers(initialUser, likedUser);
+        addMatch(unconfirmedMatch);
+        return unconfirmedMatch;
+    }
 }
