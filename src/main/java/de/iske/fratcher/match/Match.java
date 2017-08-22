59cd05b6e82c3cb09ab0aba477924365a2364ba6
André Iske <andre.iske@mailbox.org>
2017-08-22 12:13:44 +0200

    Added status property. Do some code rearrangement.

--- src/main/java/de/iske/fratcher/match/Match.java
+++ src/main/java/de/iske/fratcher/match/Match.java
@@ -1,32 +1,49 @@
 package de.iske.fratcher.match;
 
 import de.iske.fratcher.user.User;
+import de.iske.fratcher.util.Status;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 import java.time.Instant;
 
 @Entity(name = "Match_")
 public class Match {
 
     private final Instant matchingTimestamp;
+
     @Id
     @GeneratedValue
     private Long id;
 
+    @Enumerated(EnumType.ORDINAL)
+    private Status status;
+
     @ManyToOne(targetEntity = User.class)
     private User user1;
+
     @ManyToOne(targetEntity = User.class)
     private User user2;
 
     public Match() {
         matchingTimestamp = Instant.now();
+        status = Status.DEFAULT;
+
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
     }
 
     public Long getId() {
         return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public User getUser1() {
