81b3dba4d4b19d3ed55019e6e8c7c68059e1c2e6
André Iske <andre.iske@mailbox.org>
2017-09-18 21:23:31 +0200

    Added new properties for user profile.

--- src/main/java/de/iske/fratcher/user/profile/Profile.java
+++ src/main/java/de/iske/fratcher/user/profile/Profile.java
@@ -1,11 +1,38 @@
 package de.iske.fratcher.user.profile;
 
 import javax.persistence.Embeddable;
+import java.util.ArrayList;
+import java.util.List;
 
 @Embeddable
 public class Profile {
 
     private String name;
+
+    private String aboutMe;
+
+    private List<String> interest;
+
+    public Profile() {
+        interest = new ArrayList<>();
+
+    }
+
+    public String getAboutMe() {
+        return aboutMe;
+    }
+
+    public void setAboutMe(String aboutMe) {
+        this.aboutMe = aboutMe;
+    }
+
+    public List<String> getInterest() {
+        return interest;
+    }
+
+    public void setInterest(List<String> interest) {
+        this.interest = interest;
+    }
 
     public String getName() {
         return name;
