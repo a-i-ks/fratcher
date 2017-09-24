2386acb04600c83273dd4e700cac2efe1cfb0577
André Iske <andre.iske@mailbox.org>
2017-09-24 23:41:03 +0200

    Only show tags when passed parameter is not null.

--- src/main/frontend/src/components/interestsTagCloud.js
+++ src/main/frontend/src/components/interestsTagCloud.js
@@ -4,23 +4,16 @@ import {TagCloud} from "react-tagcloud";
 class InterestsTagCloud extends React.Component {
     constructor(props) {
         super(props);
-
     }
 
     render() {
-        const data = [
-            {value: "JavaScript", count: 38},
-            {value: "React", count: 30},
-            {value: "Nodejs", count: 28},
-            {value: "Express.js", count: 25},
-            {value: "HTML5", count: 33},
-            {value: "MongoDB", count: 18},
-            {value: "CSS3", count: 20}
-        ];
         return (
-            <TagCloud minSize={12}
-                      maxSize={35}
-                      tags={data}/>)
+            <div>
+                {this.props.data ?
+                    <TagCloud minSize={12}
+                              maxSize={35}
+                              tags={this.props.data}/> : ""}
+            </div>)
     }
 }
 
