f020d000b26e33a0202996cc919fc6f6044d61c1
André Iske <andre.iske@mailbox.org>
2017-09-25 14:21:09 +0200

    Fixed flattenChildren warning.

--- src/main/frontend/src/components/interestsTagCloud.js
+++ src/main/frontend/src/components/interestsTagCloud.js
@@ -6,13 +6,34 @@ class InterestsTagCloud extends React.Component {
         super(props);
     }
 
+    componentWillMount() {
+        console.log("componentWillMount()");
+        console.log(this.props.data);
+        if (this.props.data !== null) {
+            console.log("data != null");
+            const tmpKeywords = this.props.data.map(function (keyword) {
+                return {value: keyword.value, count: keyword.count, key: keyword.id};
+            });
+            console.log(tmpKeywords);
+            this.setState({
+                keywords: tmpKeywords
+            });
+            console.log("after ..");
+        } else {
+            this.setState({
+                keywords: null
+            });
+        }
+    }
+
     render() {
         return (
             <div>
-                {this.props.data ?
+                {this.state.keywords ?
                     <TagCloud minSize={12}
                               maxSize={35}
-                              tags={this.props.data}/> : ""}
+                              tags={this.state.keywords}/> :
+                    <div/>}
             </div>)
     }
 }
